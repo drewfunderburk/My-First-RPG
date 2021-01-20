4c0927ab5fcc482344145af726544b77d36b1402
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-01-20 11:13:16 -0600

    Fixed Player
    - Changed Player::takeDamage() to return Character::takeDamage() rather than it's own to prevent a stack overflow.
    - Fixed overloaded constructor
    - Implemented equipWeapon()

--- DebuggingExerciseCPP/Player.cpp
+++ DebuggingExerciseCPP/Player.cpp
@@ -1,10 +1,10 @@
-
+#include "Player.h"
 
 float Player::takeDamage(float damage)
 {
     float totalDamage = damage - m_shield.getStatusBoost();
 
-    return takeDamage(damage);
+    return Character::takeDamage(totalDamage);
 }
 
 float Player::attack(Character* other)
